d8e392cd134f377ae4409938c4d9893044fae1d2
ranasingh99 <sanketverma852@gmail.com>
2023-08-16 13:15:55 +0530

    lecture4 completed

--- lec4.js
+++ lec4.js
@@ -1,1 +1,8 @@
 // dom manipulation by apna college
+// questions to be covered - what is DOM, how it works and how js use dom
+let namsteButton = document.querySelector('button');
+namsteButton.addEventListener('click', inputMsg);
+
+function inputMsg(){
+    console.log("rana");
+}
