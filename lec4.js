d1517573f9a5412170c078dba27ad1c59585bc62
ranasingh99 <sanketverma852@gmail.com>
2023-08-16 15:41:35 +0530

    datatype and operators

--- lec4.js
+++ lec4.js
@@ -1,8 +1,17 @@
-// dom manipulation by apna college
-// questions to be covered - what is DOM, how it works and how js use dom
-let namsteButton = document.querySelector('button');
-namsteButton.addEventListener('click', inputMsg);
+//lecture 6,7,8 combined
+//basics of programming
 
-function inputMsg(){
-    console.log("rana");
-}
+var a,b,c;
+a = 5;
+b = 6;
+c = a+b;
+console.log(c);
+// javascript is the dynamically typed language -> we do notto mention the datatype of the varible
+//------------------------lecture 7-(variables)
+//A variable is a container  for a value. keywords to create(let ,var , const);
+//camelCase
+let myName
+//PascalCase
+let MyAge
+//---------------------------------------------------operators----------------
+//uranry operator,assignment operator
